c6b3e96ea79b0f46bc93f15883383de6d1a37e1e
Wade Croft <wadecroft@gmail.com>
2021-11-21 22:24:08 -0800

    add blinkjs file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var ds18b20 = require('ds18b20');
-// ds18b20.sensors(function(err, ids) {
-//   if (err) {
-//     console.log('error', err);
-//     return;
-//   } else {
-//     console.log('sensor id', ids);
-//   }
-// });
+ds18b20.sensors(function(err, ids) {
+  if (err) {
+    console.log('error', err);
+    return;
+  } else {
+    console.log('sensor id', ids);
+  }
+});
 
 const sensorId = '28-011937c40830';
 
@@ -19,4 +19,5 @@ ds18b20.temperature(sensorId, function(err, value) {
   }
 });
 
-console.log('Current temperature is' + ds18b20.temperatureSync(sensorId));
+//... sync call
+// console.log('Current temperature is' + ds18b20.temperatureSync(sensorId));
