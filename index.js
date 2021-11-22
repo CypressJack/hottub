f9052b9345abd1e9e1eabbcd0aadf292efaae496
Wade Croft <wadecroft@gmail.com>
2021-11-21 22:03:05 -0800

    add async sensor read

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var ds18b20 = require('ds18b20');
-ds18b20.sensors(function(err, ids) {
-  if (err) {
-    console.log('error', err);
-    return;
-  } else {
-    console.log('sensor id', ids);
-  }
-});
+// ds18b20.sensors(function(err, ids) {
+//   if (err) {
+//     console.log('error', err);
+//     return;
+//   } else {
+//     console.log('sensor id', ids);
+//   }
+// });
+
+const sensorId = '28-011937c40830';
 
 // ... async call
-// ds18b20.temperature('10-00080283a977', function(err, value) {
-//   console.log('Current temperature is', value);
-// });
+ds18b20.temperature(sensorId, function(err, value) {
+  if (err) {
+    console.log('error', error);
+  } else {
+    console.log('Current temperature is', value);
+  }
+});
