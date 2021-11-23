c42cce1de22f2e68186a5d3041870c99a6b15a64
wade <wadecroft@gmail.com>
2021-11-22 18:25:21 -0800

    create first loop

--- controller.js
+++ controller.js
@@ -2,10 +2,21 @@ var rpio = require('rpio');
 var ds18b20 = require('ds18b20');
 
 const sensorId = '28-011937c40830';
+let setPoint = 100;
+
+// Initialize GPIO pin as low
+rpio.open(11, rpio.OUTPUT, rpio.LOW);
 
 function manageTemp() {
-    const curTemp = ds18b20.temperatureSync(sensorId);
-    console.log(typeof curTemp);
+    const curTempC = ds18b20.temperatureSync(sensorId);
+    const curTempF = ( curTempC * (9/5) ) + 32;
+    console.log('Temp Celcius = ', curTempC);
+    console.log('Temp Fahrenheit = ', curTempF);
+    if (curTempF < setPoint){
+        rpio.write(11, rpio.HIGH);
+    } else {
+        rpio.write(11, rpio.LOW);
+    }
   }
   
   setInterval(manageTemp, 5000);
