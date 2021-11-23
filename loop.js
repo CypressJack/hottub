5d0ed4e4149e918af503d7f4a35bf5761145347a
wade <wadecroft@gmail.com>
2021-11-22 18:28:26 -0800

    edit loop

--- loop.js
+++ loop.js
@@ -1,12 +1,12 @@
 var ds18b20 = require('ds18b20');
-ds18b20.sensors(function(err, ids) {
-  if (err) {
-    console.log('error', err);
-    return;
-  } else {
-    console.log('sensor id', ids);
-  }
-});
+// ds18b20.sensors(function(err, ids) {
+//   if (err) {
+//     console.log('error', err);
+//     return;
+//   } else {
+//     console.log('sensor id', ids);
+//   }
+// });
 
 const sensorId = '28-011937c40830';
 
