9ec5b2fb695f4c4bbc83d1b028cd73b586da0084
wade <wadecroft@gmail.com>
2021-11-22 18:34:02 -0800

    read pin state

--- controller.js
+++ controller.js
@@ -7,6 +7,8 @@ let setPoint = 100;
 // Initialize GPIO pin as low
 rpio.open(11, rpio.OUTPUT, rpio.LOW);
 
+console.log('rpio pin status before = ', rpio.read(11), typeof rpio.read(11));
+
 function manageTemp() {
     const curTempC = ds18b20.temperatureSync(sensorId);
     const curTempF = ( curTempC * (9/5) ) + 32;
